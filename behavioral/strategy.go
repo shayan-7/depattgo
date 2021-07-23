42a852f24f86ce67abaaf2760a8df52d079a154b
shayan <shayan.rokrok@gmail.com>
2021-07-23 17:21:15 +0430

    Add strategy and template method

--- behavioral/strategy.go
+++ behavioral/strategy.go
@@ -1,1 +1,36 @@
 package behavioral
+
+type Operation interface {
+	Operate(n1, n2 int) int
+}
+
+type Calculator struct {
+	Operation
+
+	Num1 int
+	Num2 int
+}
+
+func NewCalculator(n1, n2 int, operation Operation) *Calculator {
+	return &Calculator{operation, n1, n2}
+}
+
+func (c *Calculator) SetOperation(operation Operation) {
+	c.Operation = operation
+}
+
+func (c *Calculator) Calculate() int {
+	return c.Operate(c.Num1, c.Num2)
+}
+
+type Addition struct{}
+
+func (a *Addition) Operate(n1, n2 int) int {
+	return n1 + n2
+}
+
+type Substraction struct{}
+
+func (s *Substraction) Operate(n1, n2 int) int {
+	return n1 - n2
+}
--- behavioral/strategy_test.go
+++ behavioral/strategy_test.go
@@ -1,1 +1,17 @@
 package behavioral
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStrategy(t *testing.T) {
+	o := Operation(&Addition{})
+	c := NewCalculator(100, 60, o)
+	assert.Equal(t, 160, c.Calculate())
+
+	o = &Substraction{}
+	c.SetOperation(o)
+	assert.Equal(t, 40, c.Calculate())
+}
